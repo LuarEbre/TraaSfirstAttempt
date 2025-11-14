b1df7de454e5349b4c6df3700c772225b11dc553
LuarEbre <raule0822@gmail.com>
2025-11-14 14:00:04 +0100

    Fix relative paths

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,20 @@ import it.polito.appeal.traci.SumoTraciConnection;
 
 void main() throws Exception {
 
-    String sumoBinary = "C:/Program Files (x86)/Eclipse/Sumo/bin/sumo-gui.exe";
-    String configFile = "C:/Users/memem/Downloads/Code/Sumo Networks/mysecondconfig.sumocfg";
+    String sumoBinary = "Resources/sumo-gui.exe";
+    String configFile = "Resources/2.sumocfg";
 
     SumoTraciConnection connection = new SumoTraciConnection(sumoBinary, configFile);
 
     // commandline commands -> --...
-    connection.addOption("delay", "200");
+    connection.addOption("delay", "30");
     connection.addOption("start", "true");
     connection.addOption("quit-on-end", "true");
 
     connection.runServer();
 
     int step = 0;
-    while (step < 50) {
+    while (step < 3600) {
 
             connection.do_timestep();
 
