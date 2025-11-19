87c704d48a46726454d7e44320d273fe2788f5e9
LeleZss <liuzzoleandro@gmail.com>
2025-11-19 13:51:19 +0100

    Erster potenzieller Methoden Test

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,35 @@ import de.tudresden.sumo.cmd.Simulation;
 import de.tudresden.sumo.util.SumoCommand;
 import it.polito.appeal.traci.SumoTraciConnection;
 
-void main() throws Exception {
+void main() {
 
-    String sumoBinary = "Resources/sumo-gui.exe";
+    String sumoBinary = "Resources/sumo.exe";
     String configFile = "Resources/2.sumocfg";
 
+    //SumoTraciConnection connection = new SumoTraciConnection(sumoBinary, configFile);
     SumoTraciConnection connection = new SumoTraciConnection(sumoBinary, configFile);
 
-    // commandline commands -> --...
-    connection.addOption("delay", "30");
-    connection.addOption("start", "true");
-    connection.addOption("quit-on-end", "true");
-
-    connection.runServer();
-
-    int step = 0;
-    while (step < 3600) {
-
+    try {
+        // Verbindung aufgebaut
+        connection.addOption("delay", "30");
+        connection.addOption("start", "true");
+        connection.addOption("quit-on-end", "true");
+        connection.runServer();
+        System.out.println("Verbindung erfolgreich: Simulationsschritt ausgeführt.");
+        int step = 1;
+        while (step <= 36) {
             connection.do_timestep();
-
-            SumoCommand simTime = Simulation.getTime();
-            System.out.println("Time: " + simTime);
-
+            //SumoCommand simTime = Simulation.getTime();
+            double timeSeconds = (double)connection.do_job_get(Simulation.getTime()); // könnte methode sein
+            System.out.println("Time: " + timeSeconds);
             step++;
+        }
+    } catch (Exception e) {
+        System.out.println("Verbindung fehlgeschlagen: " + e.getMessage());
+    } finally {
+        connection.close();
     }
 
-    connection.close();
+
 }
 
